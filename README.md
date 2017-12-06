925a18390a9fecc51cf240a0518cd497886e7fea
Natan Danous <natan.danous@gmail.com>
2017-12-06 11:38:46 +0100

    Readme update

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # MyPass
+
+### Project
+
+Building a secure password manager app.
+
+### Version 1
+
+Version 1 is a command line interface. You access your accounts with various commands.
+
+Tools    
+1. Parser or something to recognize with which argurment program is called.
+2. Read and write functions.
+
+App functionality
+
+1. mypass -init
+    ...*Creates and encrypt (with password) data file if not here. Hidden file with some infos (header).
+2. mypass -ls
+    ...*This lists all accounts without password.
+3. mypass -a
+    ...*This adds an account, will then be prompted for infos about it.
+4. mypass -s
+    ...*This searches an account by name or user. 
--- mypass.c
+++ mypass.c
@@ -5,27 +5,6 @@ Description :   MyPass is a password manager app.
 Author :        Natan Danous
 Date created :  22/11/2017
 Version :       0.0.1
-
-TODOs :
-    - Tools
-    
-        1. Parser or something to recognize with which argurment program is called.
-
-        2. Read and write functions.
-
-    - App functionality
-
-        1. mypass -init
-            Creates and encrypt (with password) data file if not here. Hidden file with some infos (header).
-
-        2. mypass -ls
-            This lists all accounts without password.
-
-        3. mypass -a
-            This adds an account, will then be prompted for infos about it.
-
-        4. mypass -s
-            This searches an account by name or user. 
 */
 
 #include <stdio.h>
